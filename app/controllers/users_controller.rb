2f3ec1afa262da18e393b3700ce427a08fa7c1c2
Benjamin Reid <ben@simpleweb.co.uk>
2015-04-16 20:53:33 +0100

    Udpate number

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def edit
+    @user = current_user
   end
 
   def update
+    current_user.phone_number = user_params[:phone_number]
+    current_user.save
+    @user = current_user
+    render :edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:phone_number)
   end
 end
