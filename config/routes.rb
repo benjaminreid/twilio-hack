aacb6ba262047dc86f205d3f8e33e2ff9a8938c2
Benjamin Reid <ben@simpleweb.co.uk>
2015-04-16 20:28:19 +0100

    Hook up the auth root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/:provider', to: 'sessions#create', as: 'auth'
   get '/thanks', to: 'pages#thanks'
   get '/thanks', to: 'pages#thanks'
   get '/number', to: 'pages#number'
