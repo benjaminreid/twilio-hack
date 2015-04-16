3782c2a9997a2138fce0b7d2f74a2de648f26568
David Darnes <me@daviddarnes.com>
2015-04-16 20:44:53 +0100

    Pretty things again, making it look like Slack

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get '/thanks', to: 'pages#thanks'
-  get '/thanks', to: 'pages#thanks'
   get '/number', to: 'pages#number'
+  get '/authorise', to: 'pages#authorise'
 end
