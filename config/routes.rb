3cd763ff147cd43fc64d0c5cd602c0bb665a5703
David Darnes <me@daviddarnes.com>
2015-04-16 20:52:26 +0100

    Resolve merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   get '/auth/:provider', to: 'sessions#create', as: 'auth'
   get '/thanks', to: 'pages#thanks'
   get '/number', to: 'pages#number'
-<<<<<<< HEAD
   get '/authorise', to: 'pages#authorise'
-=======
   resources :users, only: [:edit, :update]
->>>>>>> f275d0db7acd4d3a36691550637c8b079877b398
 end
